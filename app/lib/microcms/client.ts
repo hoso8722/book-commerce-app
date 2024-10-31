7b1863e3dc71085ef5e7d5d6e012dbaa8878cfa2
jinchangz <jinchangz1@gmail.com>
2024-10-31 22:44:33 +0900

    add customRequestInit:cache: no-store in icrocms/client.ts

--- app/lib/microcms/client.ts
+++ app/lib/microcms/client.ts
@@ -8,7 +8,10 @@ export const client = createClient({
 
 export const getAllBooks = async () => {
     const allBooks = await client.getList<BookType>({
-        endpoint: "ebook"
+        endpoint: "ebook",
+        customRequestInit: {
+          cache: "no-store"
+        },
     });
 
     return allBooks;
@@ -18,6 +21,9 @@ export const getDetailBook = async(contentId: string) => {
   const detailBook = await client.getListDetail<BookType>({
     endpoint: "ebook",
     contentId,
+    customRequestInit: {
+      cache: "no-store"
+    },
   });
 
   return detailBook;
